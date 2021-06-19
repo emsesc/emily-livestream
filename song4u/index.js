cde9485926d8154be4632a2e327f01cd405e9642
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-06-19 19:30:38 -0400

    Update index.js

--- song4u/index.js
+++ song4u/index.js
@@ -1,18 +1,75 @@
+var fetch = require('node-fetch');
 const querystring = require('querystring');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    // Parse image link (MediaUrl0) from parameters
     const queryObject = querystring.parse(req.body);
     context.log(queryObject)
     url = queryObject.MediaUrl0;
-    // the request body you are parsing looks like this: ToCountry=US&MediaContentType0=image%2Fjpeg&ToState=MI&SmsMessageSid=MM0fe83458b74a1f626eb0da4685ab28b5&NumMedia=1&ToCity=UTICA&FromZip=28394&SmsSid=MM0fe83458b74a1f626eb0da4685ab28b5&FromState=NC&SmsStatus=received&FromCity=VASS&Body=&FromCountry=US&To=%2B15869913930&ToZip=48316&NumSegments=1&MessageSid=MM0fe83458b74a1f626eb0da4685ab28b5&AccountSid=ACee62fed677d382600b621e6f24de9bb0&From=%2B19105563874&MediaUrl0=https%3A%2F%2Fapi.twilio.com%2F2010-04-01%2FAccounts%2FACee62fed677d382600b621e6f24de9bb0%2FMessages%2FMM0fe83458b74a1f626eb0da4685ab28b5%2FMedia%2FME29644fd97901859108bc35e210b588f6&ApiVersion=2010-04-01
 
-    context.log(url)
+    let resp = await fetch(url,{
+        /*The await expression causes async function execution to pause until a Promise is settled 
+        (that is, fulfilled or rejected), and to resume execution of the async function after fulfillment. 
+        When resumed, the value of the await expression is that of the fulfilled Promise*/
+        method: 'GET',
+    })
+
+    // receive the response
+    let data = await resp.arrayBuffer()
+    let result = await analyzeImage(data)
+    context.log(result)
+
+    let age = result[0].faceAttributes.age
+    context.log(age)
+    if (age > 5 && age < 25) {
+        id = "GenZ"
+    } else if (age > 24 && age < 41) {
+        id = "GenY"
+    } else if (age > 40 && age < 57) {
+        id = "GenX"
+    } else if (age > 56 && age < 76) {
+        id = "BabyBoomers"
+    } else {
+        id = "nothing"
+    }
+    context.log(id)
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: url
+        body: id
     };
+}
+
+
+async function analyzeImage(img){
+    const subscriptionKey = process.env['subscriptionkey'];
+    const uriBase = process.env['endpoint'] + '/face/v1.0/detect';
+	// env variables (similar to .gitignore/.env file) to not expose personal info
+
+    // twilio phone-numbers:update "+15869913930" --sms-url="https://twlio-test.azurewebsites.net/api/HttpTrigger1?code=P0wvX4Wrv4iTCT8zw6r356rKLTDzNLC5g5OBfrvoIiJjdUr3jNzr3A=="
+
+    let params = new URLSearchParams({
+	'returnFaceId': 'true',
+	'returnFaceAttributes': ['age']
+    })
+
+    // making the post request
+    let resp = await fetch(uriBase + '?' + params.toString(),{
+        /*The await expression causes async function execution to pause until a Promise is settled 
+        (that is, fulfilled or rejected), and to resume execution of the async function after fulfillment. 
+        When resumed, the value of the await expression is that of the fulfilled Promise*/
+        method: 'POST',
+        body: img,
+        // we want to send the image
+        headers: {
+            'Content-Type' : 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key' : process.env['subscriptionkey']
+        }
+    })
+
+    // receive the response
+    let data = await resp.json();
+
+    return data;
 }
